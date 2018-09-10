23e3bd7593d2b25864629781ca3fec9d718df5ca
VC Science Labs <vscilab@vcccd.edu>
2018-09-10 14:43:22 -0700

    Fixed Main!

--- src/main.cc
+++ src/main.cc
@@ -8,4 +8,5 @@ int main(int argc, char *argcv[]) {
 	std::cout << "Index: " << index << std::endl;
 	double doubles[] = { 3,0, 1.0, 2.0 };
 	find(0.0, doubles, 3);
+	system("PAUSE"); //This pause function should only be used for debugging. May not compile in other systems.
 }
